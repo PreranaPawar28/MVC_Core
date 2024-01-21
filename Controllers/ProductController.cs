21e934fd2107f504a5edcc4cde18f901f679b5ea
Your Name <pawarprerana9892@gmail.com>
2024-01-21 17:40:48 +0530

    first commit

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Core.Models;
+
+namespace MVC_Core.Controllers
+{
+    public class ProductController : Controller
+    {
+        private static List<Product> products = new List<Product>();
+        public ActionResult Index()
+        {
+            return View(products);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                product.ProductID = products.Count + 1;
+                products.Add(product);
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var product = products.Find(p => p.ProductID == id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                var existingProduct = products.Find(p => p.ProductID == product.ProductID);
+                if (existingProduct != null)
+                {
+                    existingProduct.ProductName = product.ProductName;
+                    existingProduct.ProductDescription = product.ProductDescription;
+                }
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+
+
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVC_Core.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+
+        [Required(ErrorMessage = "Product Name is required.")]
+        public string ProductName { get; set; }
+
+        [StringLength(100, ErrorMessage = "Product Description cannot exceed 100 characters.")]
+        public string ProductDescription { get; set; }
+    }
+}
